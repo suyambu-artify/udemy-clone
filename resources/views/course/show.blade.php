c98b9d7d3dc6845bce392de56b4dbb460a4c6787
thomas1117 <thomasshannon1117@yahoo.com>
2017-05-30 23:38:25 -0700

    TYPO
    - fixed migration file name and added lecture lesson and review models

--- resources/views/course/show.blade.php
+++ resources/views/course/show.blade.php
@@ -1,9 +1,20 @@
 @extends('layouts.my-courses-detail')
 @section('content')
-	<div class="bg-gray">
+	<div class="bg-gray text-white">
+
+		{{ json_encode($course) }}
 		<div class="container">
 			<div class="row pad">
-				<h2>{{ $course->title }}</h2>
+				<div class="col-lg-6">
+					<img src="{{ $course->image }}" alt="">
+				</div>
+				<div class="col-lg-6">
+					<h2>{{ $course->title }}</h2>
+					<a class="btn btn-primary">Continue to lecture 12</a>
+					<div class="rating pad-sm">
+						<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
