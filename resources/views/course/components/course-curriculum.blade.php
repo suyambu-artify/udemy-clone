a0da96c17ec56360ef706fb5beaddbf623415b14
thomas1117 <thomasshannon1117@yahoo.com>
2017-06-03 14:38:24 -0700

    added more to course seeder

--- resources/views/course/components/course-curriculum.blade.php
+++ resources/views/course/components/course-curriculum.blade.php
@@ -1,10 +1,21 @@
 <div class="course-lectures">
 	@foreach($course->lectures as $lecture)
-	<div class="lecture"><a href="#lesson-{{ $lecture->id }}" data-toggle="collapse">{{ $lecture->title }}</a></div>
+	<div class="lecture">	
+		<div class="row">
+			<div class="col-xs-8">
+				<a href="#lesson-{{ $lecture->id }}" data-toggle="collapse">
+					<strong>{{ $lecture->title }}</strong>
+				</a>
+			</div>
+			<div class="col-xs-4">
+				<span>{{ count($lecture->lessons) }} Lectures</span>
+			</div>
+		</div>
+	</div>
 
 	<div id="lesson-{{ $lecture->id }}" class="course-lesson collapse">
 		@foreach($lecture->lessons as $lesson)
-			<p class="lesson">{{ $lesson->title }}</p>
+			<div class="lesson"><span>{{ $lesson->title }}</span> <span class="lesson-time">{{ $lesson->time }}</span></div>
 		@endforeach
 	</div>
 	@endforeach
